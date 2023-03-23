831c8117f7039c23df4bf6d842b8a216b542fdf0
Kahbelan <2717909K@student.gla.ac.uk>
2023-03-23 14:31:40 +0800

    Update user.controller.js

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -17,63 +17,57 @@ exports.teacherBoard = (req, res) => {
   res.status(200).send("Teacher Content.");
 };
 
-<<<<<<< HEAD
-const db = require("../models");
-
-const User = db.user;
-
 exports.userBoard = async (req, res) => {
   try {
     const users = await User.findAll();
     res.status(200).render("userBoard", { users });
   } catch (err) {
     console.log(err);
-    res
-      .status(500)
-      .send({
-        message: err.message || "Error occurred while retrieving users",
-      });
+    res.status(500).send({
+      message: err.message || "Error occurred while retrieving users",
+    });
   }
 };
-=======
 exports.editProfile = (req, res) => {
-  const { id, 
-    name, 
-    contactNumber, 
-    address, 
+  const {
+    id,
+    name,
+    contactNumber,
+    address,
     modeTeaching,
     subjectPreferences,
     hourly,
-} = req.body;
+  } = req.body;
 
   User.update(
     {
       name,
       contactNumber,
-      address, 
+      address,
       modeTeaching,
       subjectPreferences,
       hourly,
     },
     {
-    where: {
-      id: id
+      where: {
+        id: id,
+      },
     }
-  }).then((num) => {
-    if (num == 1) {
-      res.send({
-        message: "Profile updated successfully.",
+  )
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Profile updated successfully.",
+        });
+      } else {
+        res.status(400).send({
+          message: "Failed to update profile.",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Internal server error.",
       });
-    } else {
-      res.status(400).send({
-        message: "Failed to update profile.",
-      });
-    }
-  })
-  .catch((err) => {
-    res.status(500).send({
-      message: "Internal server error.",
     });
-  });
 };
->>>>>>> 7c25cf21cf44dd7fdf40805db57ba8b390bcbe8d
